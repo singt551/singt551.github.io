a49acfcb56ac7bad852fc7cd553accfcb567c196
singt551 <singt551@newschool.edu>
2023-03-09 10:53:40 -0500

    project 2 connected to main page

---
+++ studio/demo/java.js
@@ -0,0 +1,19 @@
+let speak = "woof"
+
+console.log(speak)
+
+let char = speak[0]
+console.log(speak.length)
+console.log(char)
+
+let ask = "hi"
+console.log(ask)
+
+let scream = "hello!"
+console.log(scream)
+
+let howl = "heyyy!!"
+console.log(howl)
+
+let humanTalk = ask + scream + howl
+console.log(humanTalk)
