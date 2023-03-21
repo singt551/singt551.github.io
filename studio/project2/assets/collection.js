03dc57bd5cc1b0f194dbe1fa828cd652057593c3
singt551 <singt551@newschool.edu>
2023-03-20 22:18:10 -0400

    trial

--- studio/project2/assets/collection.js
+++ studio/project2/assets/collection.js
@@ -1,44 +1,50 @@
-// Function to render your items
-const renderItems = (collection) => {
-	// The `ul` where the items will be inserted
-	const collectionList = document.getElementById('collection')
+// // Function to render your items
+// const renderItems = (collection) => {
+// 	// The `ul` where the items will be inserted
+// 	const collectionList = document.getElementById('collection')
 
 
-	// Loop through each item in the collection array
-	collection.forEach(item => {
-		const listItem = document.createElement('li') // Make the `li`
+// 	// Loop through each item in the collection array
+// 	collection.forEach(item => {
+// 		const listItem = document.createElement('li') // Make the `li`
 
 
-		// You can make each element inside of that…
-		const itemTitle = document.createElement('h2') // Make an `h2`
-		itemTitle.innerHTML = item.title // Put the JSON title inside
-		listItem.appendChild(itemTitle) // And add it to the `li`!
+// 		// You can make each element inside of that…
+// 		const itemTitle = document.createElement('h2') // Make an `h2`
+// 		itemTitle.innerHTML = item.title // Put the JSON title inside
+// 		listItem.appendChild(itemTitle) // And add it to the `li`!
 
-		const itemImage = document.createElement('img') // And an image
-		itemImage.src = item.posterImage // Set the `src` attribute from the JSON
-		listItem.appendChild(itemImage) // And add that too
+// 		const itemImage = document.createElement('img') // And an image
+// 		itemImage.src = item.posterImage // Set the `src` attribute from the JSON
+// 		listItem.appendChild(itemImage) // And add that too
 
 
-		// This can get annoying, so we can use “template literals” instead
-		const itemDetails =
-			`
-				<p>Released in <time>${item.year}</time></p>
-				<p><em>${item.runTime}</em></p>
-				<a href="${item.imdbLink}">
-					<p>${item.imdbRating} / 10 →</p>
-				</a>
-			`
-		listItem.insertAdjacentHTML('beforeend', itemDetails) // Which can we then insert
+// 		// This can get annoying, so we can use “template literals” instead
+// 		const itemDetails =
+// 			`
+// 				<p>Released in <time>${item.year}</time></p>
+// 				<p><em>${item.runTime}</em></p>
+// 				<a href="${item.imdbLink}">
+// 					<p>${item.imdbRating} / 10 →</p>
+// 				</a>
+// 			`
+// 		listItem.insertAdjacentHTML('beforeend', itemDetails) // Which can we then insert
 
-		// You can build logic from your data, too
-		if (!item.alsoWriter) { // If this is `false`
-			listItem.classList.add('faded') // Add this class to the whole `li`
-		}
+// 		// You can build logic from your data, too
+// 		if (!item.alsoWriter) { // If this is `false`
+// 			listItem.classList.add('faded') // Add this class to the whole `li`
+// 		}
 
-		collectionList.appendChild(listItem) // Then add the whole `li` into the `ul`
-	})
-}
+// 		collectionList.appendChild(listItem) // Then add the whole `li` into the `ul`
+// 	})
+// }
 
+document.querySelectorAll("img").forEach((item) => {
+	item.addEventListener("click", (event) => {
+	  const image = event.target.getAttribute("data-src");
+	  event.target.setAttribute("src", image);
+	});
+  });
 
 
 // Fetch gets your JSON file…
